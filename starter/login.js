b7a6f713fc17a79413b12f4eba1003b548854e64
Azizjon <aliqulovazizjon68@gmail.com>
2022-04-08 18:36:01 +0500

    starter

---
+++ starter/login.js
@@ -0,0 +1,13 @@
+// let b = 100;
+// let a = 10000;
+// let c = 12000;
+// // console.log(b)
+// export default function (login, parol) {
+//   if (login === 'azizjon' && parol === 1111) {
+//     console.log('Siz tizimga kirdingiz');
+//   } else console.log('Siz tizimga kira olmadingiz');
+// }
+
+// export { b, a, c };
+let a = 100;
+module.exports = { a };
---
+++ starter/script.js
@@ -0,0 +1,27 @@
+// import sana from './login.js';
+// let b = 1000;
+// sana('umid', 1111);
+// import * as hammasi from './login.js';
+// console.log(hammasi);
+// console.log(a);
+
+// // console.log('salom dunyo');
+
+// // let mamlakat = await fetch('https://restcountries.com/v2/name/uzbekistan');
+// // console.log(await mamlakat.json());
+
+// let sot = function () {
+//   let cart = [];
+//   let maxsulotSoni = 100;
+//   let ishchilarSoni = 10;
+//   function sotibOl(nom1, soni) {
+//     console.log(`siz ${nom1} li ${soni} li maxsulot sotib oldingiz`);
+//   }
+//   return sotibOl;
+// };
+
+// let sana = sot();
+// console.log(sana);
+// let ishla = sana('olma', 100);
+const login = require('./login.js');
+console.log(login);
